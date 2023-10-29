ab52950f971e418de24e7e0a3a8eafd3bdeda021
“Irina <irina.osipova2012@gmail.com>
2023-10-29 21:37:08 +0100

    refactor: replace App with class component

--- RS_React/src/App.tsx
+++ RS_React/src/App.tsx
@@ -1,8 +1,11 @@
+import { Component, ReactNode } from 'react';
 import './App.css';
 import Wrapper from './components/Wrapper';
 
-function App() {
-  return <Wrapper />;
+class App extends Component {
+  render(): ReactNode {
+    return <Wrapper />;
+  }
 }
 
 export default App;
