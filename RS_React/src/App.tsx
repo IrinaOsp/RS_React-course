8912a01dc20aec6a4eb5e10329e59851b09686e9
“Irina <irina.osipova2012@gmail.com>
2023-11-02 12:54:04 +0100

    refactor: migrate App to func

--- RS_React/src/App.tsx
+++ RS_React/src/App.tsx
@@ -1,16 +1,14 @@
-import { Component, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import './App.css';
 import Wrapper from './components/Wrapper';
 import ErrorBoundary from './components/ErrorBoundary';
 
-class App extends Component {
-  render(): ReactNode {
-    return (
-      <ErrorBoundary>
-        <Wrapper />
-      </ErrorBoundary>
-    );
-  }
+function App(): ReactNode {
+  return (
+    <ErrorBoundary>
+      <Wrapper />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
