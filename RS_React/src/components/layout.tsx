2ba183cdda5fdd85d4130c17b138816054b23d14
“Irina <irina.osipova2012@gmail.com>
2023-11-23 18:42:16 +0100

    feat: migrate SearchForm and 404

--- RS_React/src/components/layout.tsx
+++ RS_React/src/components/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Pokemon search',
@@ -8,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body>
         <main>{children}</main>
       </body>
---
+++ RS_React/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { SearchForm } from '../components/SearchForm/SearchForm';
+// import { Pagination } from './components/Pagination/Pagination';
+// import { SearchResults } from './components/SearchResults/SearchResults';
+
+export default function Home() {
+  return (
+    <SearchForm />
+    // <Pagination />
+    // <SearchResults />
+  );
+}
