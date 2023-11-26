cc81caf962205b0669134a9a01b4857e930eff97
“Irina <irina.osipova2012@gmail.com>
2023-11-27 00:29:44 +0100

    fix: test searchform

--- RS_React/src/components/SearchForm/SearchForm.test.tsx
+++ RS_React/src/components/SearchForm/SearchForm.test.tsx
@@ -1,7 +1,6 @@
 import { expect, describe, it, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { SearchForm } from './SearchForm';
-import mockRouter from 'next-router-mock';
 
 describe('SearchForm', async () => {
   it('search form takes input event value', async () => {
@@ -22,13 +21,5 @@ describe('SearchForm', async () => {
     fireEvent.change(input, { target: { value: searchText } });
     fireEvent.click(button);
     expect(input.value).toBe(searchText);
-  });
-
-  it('gets the value from the query params upon mounting', async () => {
-    mockRouter.push('?search=ditto');
-
-    render(<SearchForm />);
-    const input = screen.getByRole('textbox') as HTMLInputElement;
-    expect(input.value).toEqual('ditto');
   });
 });
