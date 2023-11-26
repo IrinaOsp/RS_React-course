d8d81aea04f023e29c708ca8d85f6808a62ba9e7
“Irina <irina.osipova2012@gmail.com>
2023-11-26 21:13:23 +0100

    test: adjust Card test

--- RS_React/src/components/Card/Card.test.tsx
+++ RS_React/src/components/Card/Card.test.tsx
@@ -1,37 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Card from './Card';
 import '../../mocks/server';
 import { ISearchArrayItem } from '../../types/types';
-import { Provider } from 'react-redux';
-import { store } from '../../state/store';
 
 const mockData: ISearchArrayItem = {
   name: 'testPokemon',
   url: 'https://pokeapi.co/api/v2/pokemon/1/',
 };
 
-const MockCard = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Card {...mockData} />
-      </BrowserRouter>
-    </Provider>
-  );
-};
-
 describe('Card', () => {
   it(`renders relevant card data`, () => {
-    render(<MockCard />);
+    render(<Card {...mockData} />);
     expect(screen.getByText(/name/i)).toHaveTextContent(`Name: ${mockData.name}`);
   });
-  // describe('Card behavior on click', () => {
-  //   it('triggers an additional API call onclick', () => {
-  //     render(<MockCard />);
-  //     fireEvent.click(screen.getByText(`Name: ${mockData.name}`));
-  //     expect(window.location.pathname).(`/${mockData.id}`);
-  //   });
-  // });
+  it('triggers an additional API call onclick', () => {
+    render(<Card {...mockData} />);
+    fireEvent.click(screen.getByText(`Name: ${mockData.name}`));
+    expect(window.location.pathname).toBe('/');
+  });
 });
--- RS_React/src/components/Card/Card.tsx
+++ RS_React/src/components/Card/Card.tsx
@@ -6,7 +6,6 @@ import styles from './Card.module.css';
 export default function Card(data: ISearchArrayItem | ISearchResponseItemDetailed) {
   // id from url https://pokeapi.co/api/v2/pokemon/1/
   const id = 'url' in data ? data.url.substring(baseURL.length).slice(0, -1) : data.id;
-
   return (
     <Link href={`/${id}`} style={{ color: 'inherit' }}>
       <div className={styles.searchItem}>
